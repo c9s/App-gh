ff23a5852d22bcc66a632c7a2d4fea07fc3223ad
c9s <cornelius.howl@gmail.com>
2010-09-13 05:08:27 +0800

    add verbose flag

--- lib/App/gh/Command/Cloneall.pm
+++ lib/App/gh/Command/Cloneall.pm
@@ -7,11 +7,10 @@ use App::gh::Utils;
 use LWP::Simple qw(get);
 use JSON;
 
-sub options {
-    (
+sub options { (
+        "verbose" => "verbose",
         "into=s" => "into"
-    )
-}
+    ) }
 
 sub run {
     my $self = shift;
@@ -27,15 +26,13 @@ sub run {
 
     die 'need id' unless $acc;
 
-    _info "Getting repository list from github: $acc";
+    _info "Getting repository list from github: $acc" if $self->{verbose};
     my $json = get 'http://github.com/api/v2/json/repos/show/' . $acc;
     my $data = decode_json( $json );
 
     _info "Will clone repositories below:";
-    for my $repo ( @{ $data->{repositories} } ) {
-        print "  " . $repo->{name} . "\n";
-    }
-
+    print join " " , map { $_->{name} } @{ $data->{repositories} };
+    print "\n";
 
     for my $repo ( @{ $data->{repositories} } ) {
         my $repo_name = $repo->{name};
@@ -49,7 +46,7 @@ sub run {
         elsif( $attr eq 'ssh' ) {
             $uri = sprintf "git\@github.com:%s/%s.git" , $acc , $repo_name;
         }
-        print $uri . "\n";
+        print $uri . "\n" if $self->{verbose};
 
         if( -e $local_repo_name ) {
             print "Updating " . $local_repo_name . " ...\n";
