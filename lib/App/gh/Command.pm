3e5681ab2c908ada5445f7dcca6f98cdd1f9b4b0
c9s <cornelius.howl@gmail.com>
2010-09-29 13:10:01 +0800

    clean up help messages

--- lib/App/gh/Command.pm
+++ lib/App/gh/Command.pm
@@ -26,87 +26,33 @@ sub global_help {
     print <<'END';
 App::gh
 
-show help message;
 
-    $ gh help
+help                   
+    - show help message
 
-* list:
+list [userid]          
+    - list all repository of an user:
 
-    list all repository of c9s:
+clone [userid] [repo] ([http|ro|ssh])
+    - clone repository from an user
 
-        $ gh list c9s
+search [keyword] 
+    - search repository:
 
-    if you want text wrapped:
+all [userid]
+    - to clone all repository of an user:
 
-        $ WRAP=1 gh list c9s
+fork [userid] [repo]
+    - to fork project:
 
-* clone:
-
-    clone Plack repository from miyagawa:
-
-        $ gh clone miyagawa/Plack   # default: read-only 
-
-    or:
-
-        $ gh clone miyagawa Plack
-
-        $ gh clone gugod Social http
-
-        $ gh clone clkao Web-Hippie ro
-
-    clone from read-only uri:
-
-        $ gh clone miyagawa/Plack ro 
-
-    clone from ssh uri:
-
-        $ gh clone miyagawa/Plack ssh  
-
-* search:
-
-    search repository:
-
-        $ gh search Plack
-
-* cloneall:
-
-    to clone all repository of miyagawa:
-
-        $ gh cloneall miyagawa 
-
-        $ gh cloneall clkao ro  # read-only
-
-* fork;
-
-    to fork project:
-
-        $ gh fork clkao AnyMQ
-
-    to fork current project:
+fork
+    - to fork current project:
         
-        $ cd clkao/AnyMQ
-        $ gh fork
+network 
+    -  to show fork network:
 
-* network:
-
-    to show fork network:
-
-        $ cd App-gh/
-        $ gh network
-            c9s/App-gh - watchers(4) forks(1)
-          gugod/App-gh - watchers(1) forks(0)
-
-* pull from other's fork:
-
-    pull from gugod/project.git branch master (default):
-
-        $ cd project
-        $ gh pull gugod           
-
-    pull from gugod/project.git branch feature:
-
-        $ cd project
-        $ gh pull gugod feature    
+pull [userid] ([branch])
+    - pull from other's fork:
 
 END
 }
