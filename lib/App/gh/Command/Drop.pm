ae3181d91353a4452376dc9a10e1cac5da6713b4
c9s <cornelius.howl@gmail.com>
2010-10-17 01:15:20 +0900

    add drop command

---
+++ lib/App/gh/Command/Drop.pm
@@ -0,0 +1,38 @@
+package App::gh::Command::Drop;
+use warnings;
+use strict;
+use base qw(App::gh::Command);
+use App::gh::Utils;
+use LWP::Simple qw(get);
+use JSON;
+
+=head1 USAGE
+
+    $ gh drop [repository]
+
+=cut
+
+sub run {
+    my ($self,$repo) = @_;
+    my $auth = get_github_auth();
+
+    unless( $auth ) {
+        die "Github authtoken not found. Can not fork repository.\n";
+    }
+    die unless $repo;
+
+    # repos/delete/:user/:repo
+    my $uri = sprintf( qq{repos/delete/%s/%s?login=%s&token=%s}, $auth->{user}, $repo, $auth->{user}, $auth->{token} );
+    my $ret = api_request($uri);
+    my $delete_token = $ret->{delete_token};
+    $uri .= '&delete_token=' . $delete_token;
+    $ret = api_request($uri);
+    print $ret->{status} , "\n";
+    return;
+}
+
+
+
+
+
+1;
