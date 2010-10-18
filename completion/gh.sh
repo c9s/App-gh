39642f22c5a4cb0087459da3237768f826389fbc
c9s <cornelius.howl@gmail.com>
2010-10-19 02:22:03 +0900

    Support some options

--- completion/gh.sh
+++ completion/gh.sh
@@ -11,7 +11,7 @@ function _gh()
         fi
         COMPREPLY=( $( compgen -W "$gh_commands" -- $cur ) )
         return 0
-    elif (($COMP_CWORD == 2)); then
+    elif (($COMP_CWORD == 2 )); then
         local subcmd=${COMP_WORDS[1]}
         if [[ $subcmd == "fork" || $subcmd == "pull" ]] ; then
             local gh_forks=$(gh network --id)
@@ -22,5 +22,25 @@ function _gh()
             return 0
         fi
     fi
+    if [[ $COMP_WORDS > 1 && $cur =~ ^-- ]] ; then
+        local subcmd=${COMP_WORDS[1]}
+        local cmdoptions=""
+        case $subcmd in
+            clone|all)
+                cmdoptions='--ssh --ro --http --https'
+                ;;
+            pull)
+                cmdoptions='--merge --branch'
+                ;;
+            network)
+                cmdoptions='--id'
+                ;;
+        esac
+        if [[ -n $cmdoptions ]] ; then
+            COMPREPLY=( $( compgen -W "$cmdoptions" -- $cur ) )
+        fi
+        return 0
+    fi
 }
+
 complete -F _gh gh
