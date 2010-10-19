f680f2adb344a4f065c1f9773631d13ba3af7163
c9s <cornelius.howl@gmail.com>
2010-10-19 17:10:48 +0900

    Implement commit command.

---
+++ lib/App/gh/Command/Commit.pm
@@ -0,0 +1,71 @@
+package App::gh::Command::Commit;
+use warnings;
+use strict;
+use utf8;
+use warnings;
+use strict;
+use base qw(App::gh::Command);
+use File::Path qw(mkpath);
+use App::gh::Utils;
+use LWP::Simple qw(get);
+use JSON;
+use File::Temp;
+
+# use IO::Pager;
+use Term::ReadLine;
+
+sub run {
+    my ($self) = @_;
+
+    system( qq(git status) );
+
+    # print qx(git diff --color=auto);
+    my $term = Term::ReadLine->new('Simple');
+    my $prompt = "Show Diff(d), Status(s), Commit(c), Quit(q) :";
+
+    my $OUT = $term->OUT || \*STDOUT;
+    my $res; 
+    while ( defined ($res = $term->readline($prompt)) ) {
+        chomp($res);
+        if ( $res =~ /^d/ ) {
+            system( qq(git diff --color=always) );
+        }
+        elsif( $res =~ /^s/ ) {
+            system( qq(git status) );
+        }
+        elsif( $res =~ /^c/ ) {
+            # read commit messages
+
+            print "Please enter commit messages below (empty line to finish):\n";
+            my @lines = qw();
+            my $line;
+            while( defined ($line = $term->readline(">> ")) ) {
+                last unless $line;
+                chomp $line;
+                push @lines , $line;
+            }
+
+            last unless @lines;
+
+            use File::Temp qw(tempfile);
+            my ($fh, $filename) = tempfile( ".gh_commit_XXXX" , SUFFIX => '.msg');
+            print $fh join "\n",  @lines;
+            system( "git commit -a -F " . $filename ) == 0 or 
+                die "Commit message saved to $filename.\n";
+
+            unlink( $filename );
+            last;
+        }
+        elsif( $res =~ /^q/ ) {
+            print "Skipped\n";
+            last;
+        }
+# #         my $res = eval($_);
+# #         warn $@ if $@;
+# #         print $OUT $res, "\n" unless $@;
+#         # $term->addhistory($_) if /\S/;
+    }
+
+}
+
+1;
