243b106819573d3c44e49249e2f867baa9aea2a6
Ryan C. Thompson <rct@thompsonclan.org>
2011-01-24 09:45:10 -0800

    Fix inverted logic (add the missing logical NOT)

--- lib/App/gh/Command/Import.pm
+++ lib/App/gh/Command/Import.pm
@@ -63,7 +63,7 @@ sub run {
                          "git\@github.com:${gh_id}/${reponame}.git");
 
     # Only set up branch remote if it isn't already set up.
-    if ($local_repo->config('branch.master.remote')) {
+    if (! $local_repo->config('branch.master.remote')) {
         print "Setting up remote [$remote] for master branch.\n";
         $local_repo->command('config', 'branch.master.remote', "$remote");
         $local_repo->command('config', 'branch.master.merge', 'refs/heads/master');
