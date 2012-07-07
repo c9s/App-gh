c9s/App-gh

commit 2e51563772baf1cca8e75fc9783283eb00b354a5

Add note for fetch alias

lib/App/gh/Command.pm
@@ -20,8 +20,14 @@ sub alias { (
         "se" => "search",
         "up" => "update",
 
+        # TODO: 
+        #   fetch should only do git fetch 
+        #   pull should only do git pull
+        #   should wrap these commands with github features
+        #   
+        #   https://github.com/c9s/App-gh/pull/39
         "fetch" => "pull",
-        ) }
+    )
 
 sub invoke {
     my ($pkg, $cmd, @args) = @_;

commit 1424699ca5726e0fac94b91f34ae14391699fb4a

fix syntax error introduced in 2e515637

lib/App/gh/Command.pm
@@ -27,7 +27,7 @@ sub alias { (
         #   
         #   https://github.com/c9s/App-gh/pull/39
         "fetch" => "pull",
-    )
+    ) }
 
 sub invoke {
     my ($pkg, $cmd, @args) = @_;