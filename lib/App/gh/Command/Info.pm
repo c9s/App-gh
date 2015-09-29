d9b8a279ad2c743640b4860247e6c433e2207fd7
Yanick Champoux <yanick@babyl.dyndns.org>
2015-09-28 20:58:44 -0400

    Fix 'gh info'

--- lib/App/gh/Command/Info.pm
+++ lib/App/gh/Command/Info.pm
@@ -8,11 +8,16 @@ use App::gh::Utils;
 
 sub parse_uri {
     my ($uri) = @_;
-    if ( $uri =~ m{(git|https?)://github.com/(.*?)/(.*?).git} ) {
-        return ($2,$3,$1);
-    } elsif ( $uri =~ m{git\@github.com:(.*?)/(.*?).git} ) {
-        return ($1,$2,'git');
-    }
+
+    return ($2,$3,$1)
+        if $uri =~ m{(git|https?)://github.com/(.*?)/(.*?).git};
+
+    return ($1,$2,'git')
+        if $uri =~ m{git\@github.com:(.*?)/(.*?).git};
+
+     return ( $1, $2 )
+        if $uri =~ m#([^/:]+)/([^/]+)(?:\.git)?$#;
+
     return undef;
 }
 
@@ -26,11 +31,13 @@ sub get_remote {
 
 sub run {
     my $self = shift;
-    # my $gh_id = App::gh->config->github_id();
-    my $remote = $self->get_remote();
-    die "Remote not found\n." unless $remote;
-    my ( $user, $repo, $uri_type ) = parse_uri( $remote->{url} );
-    my $ret = App::gh->api->repo_info( $user, $repo );
+
+    my $remote = $self->get_remote
+        or die "Remote not found\n.";
+
+    my ( $user, $repo ) = parse_uri( $remote->{url} );
+
+    my $ret = App::gh->github->repos->get( $user, $repo );
     App::gh::Utils->print_repo_info( $ret );
 }
 
